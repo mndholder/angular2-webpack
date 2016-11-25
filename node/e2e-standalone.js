0118153f60831cb24c50aafa3707eccba258a4e6
mndholder <mndholder@gmail.com>
2016-11-25 15:16:59 +0300

    Fixed standalone e2e tests

--- node/e2e-standalone.js
+++ node/e2e-standalone.js
@@ -6,7 +6,7 @@ const   APP_HOST = 'http://localhost',
         APP_URI = `${APP_HOST}:${APP_PORT}`;
 
 const   BUILD_CMD = 'npm run build',
-        SERVE_DIST_CMD = `npm run serve -- -s -p ${APP_PORT}`,
+        SERVE_DIST_CMD = `npm run server -- -s -p ${APP_PORT}`,
         E2E_CMD = `npm run e2e -- --baseUrl=\'${APP_URI}\'/`;
 
 // Synchronously build the app into dist/
